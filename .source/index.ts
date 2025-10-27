2e36a76b97714475a868f645ae6dac482114c4f3
fusionfx1 <85038386+fusionfx1@users.noreply.github.com>
2025-10-28 00:36:10 +0700

    Added index.ts

---
+++ .source/index.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck -- skip type checking
+import * as docs_5 from "../blog/content/react-portfolio-templates.mdx?collection=docs&hash=1761586397056"
+import * as docs_4 from "../blog/content/react-native-libraries.mdx?collection=docs&hash=1761586397056"
+import * as docs_3 from "../blog/content/react-landing-page-templates.mdx?collection=docs&hash=1761586397056"
+import * as docs_2 from "../blog/content/react-animation-libraries.mdx?collection=docs&hash=1761586397056"
+import * as docs_1 from "../blog/content/nextjs-portfolio-templates.mdx?collection=docs&hash=1761586397056"
+import * as docs_0 from "../blog/content/21-best-free-react-components.mdx?collection=docs&hash=1761586397056"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"21-best-free-react-components.mdx","absolutePath":"/home/project/blog/content/21-best-free-react-components.mdx"}, data: docs_0 }, { info: {"path":"nextjs-portfolio-templates.mdx","absolutePath":"/home/project/blog/content/nextjs-portfolio-templates.mdx"}, data: docs_1 }, { info: {"path":"react-animation-libraries.mdx","absolutePath":"/home/project/blog/content/react-animation-libraries.mdx"}, data: docs_2 }, { info: {"path":"react-landing-page-templates.mdx","absolutePath":"/home/project/blog/content/react-landing-page-templates.mdx"}, data: docs_3 }, { info: {"path":"react-native-libraries.mdx","absolutePath":"/home/project/blog/content/react-native-libraries.mdx"}, data: docs_4 }, { info: {"path":"react-portfolio-templates.mdx","absolutePath":"/home/project/blog/content/react-portfolio-templates.mdx"}, data: docs_5 }]);
+export const meta = _runtime.meta<typeof _source.meta>([]);
